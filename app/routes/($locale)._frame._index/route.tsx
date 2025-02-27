dc875a27ad4196d6ccbf6f675ad03ad34ae0b038
dasabot <dasabot@icloud.com>
2025-02-27 19:57:36 +0200

    remove console log

--- app/routes/($locale)._frame._index/route.tsx
+++ app/routes/($locale)._frame._index/route.tsx
@@ -43,7 +43,7 @@ export async function loader({
 
 export default function Homepage() {
   const { sections } = useLoaderData<typeof loader>()
-  console.log('sections:', sections)
+  
   return (
     <>
       {sections?.map((section) =>
